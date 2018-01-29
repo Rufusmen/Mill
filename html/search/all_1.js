fc54f25aac4edd1d38c1f04d506106ea4fa573c2
Rufusmen <rafusmen@gmail.com>
2018-01-29 23:52:33 +0100

    v1.1
    Adds Makefile hot bug fix

---
+++ html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['my_5fbutton',['my_button',['../structmy__button.html',1,'']]]
+];
---
+++ html/search/classes_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['pipes',['pipes',['../structpipes.html',1,'']]],
+  ['player',['player',['../structplayer.html',1,'']]]
+];
